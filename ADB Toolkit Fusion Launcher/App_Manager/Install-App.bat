e6dc6542c468a0e175c959f7fed3feca014d9035
&Undefined&Z -ICS^ <51278093+SignedZ@users.noreply.github.com>
2020-04-16 14:31:37 +0800

    Add Gui
    加队列安装和一次一次安装（可能没人选第二个了草）

--- ADB Toolkit Fusion Launcher/App_Manager/Install-App.bat
+++ ADB Toolkit Fusion Launcher/App_Manager/Install-App.bat
@@ -1,9 +1,36 @@
 @echo off
 echo 服务启动中..
 %adb% start-server
-:start
 cls
+:mui
+echo 1]一次一次安装
+echo 2]队列安装
+echo 3]打开已生成的队列安装脚本
+choice /n /c:12 /m "键入功能数字...>"
+if '%ERRORLEVEL%'=='1' goto x_mui
+if '%ERRORLEVEL%'=='2' md temp&echo %adb% start-server>temp\install.bat&echo cls>>temp\install.bat&goto y_mui
+if '%ERRORLEVEL%'=='3' goto list_install
+goto mui
+
+:x_mui
 set /p APK=apk_File:
-echo [%date%-%time%]Action:application.install(file:%apk%)>>D:\ADB_Tools.log
+echo [%date%-%time%]Action:application.install.once(file:%apk%)>>D:\ADB_Tools.log
 %adb% install %APK%
-goto start
+goto x_mui
+
+:y_mui
+cls
+echo 该界面会一直重复 直到手动关闭窗口
+echo 如需安装请重新在ADB Toolkit里打开该功能
+set /p APK=apk_File:
+echo 写入脚本中
+echo %adb% install %APK%>>temp\install.bat
+echo [%date%-%time%]Action:application.list.added(file:%apk%)>>D:\ADB_Tools.log
+goto y_mui
+
+:list_install
+start temp\install.bat
+echo [%date%-%time%]Action:application.list.install>>D:\ADB_Tools.log
+rd /s /q  temp
+echo [%date%-%time%]Action:file.deleted()>>D:\ADB_Tools.log
+goto mui
